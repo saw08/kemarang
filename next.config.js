4a6641e4b360691972a916c7eb2aeee478a39180
saw08 <kuliah.1182@gmail.com>
2023-01-09 12:28:29 +0700

    first commit5

--- next.config.js
+++ next.config.js
@@ -1,9 +1,9 @@
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   reactStrictMode: true,
-//   swcMinify: true,
-// }
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+}
 
 
 
-// module.exports = nextConfig
+module.exports = nextConfig
